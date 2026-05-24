51a821d0327f2c3c44b01ca5c3ef4cc0a1294351
abcd <abcd@192.168.0.100>
2026-05-24 17:03:52 +0800

    feat: 实现除法拆解器(乘一法+LMDI)

--- metric_analyzer/decomposers/__init__.py
+++ metric_analyzer/decomposers/__init__.py
@@ -3,6 +3,7 @@
 from metric_analyzer.decomposers.addition import AdditionDecomposer
 from metric_analyzer.decomposers.dual_factor import DualFactorDecomposer
 from metric_analyzer.decomposers.multiplication import MultiplicationDecomposer
+from metric_analyzer.decomposers.division import DivisionDecomposer
 from metric_analyzer.decomposers.subtraction import SubtractionDecomposer
 
 __all__ = [
@@ -10,4 +11,5 @@ __all__ = [
     "SubtractionDecomposer",
     "MultiplicationDecomposer",
     "DualFactorDecomposer",
+    "DivisionDecomposer",
 ]
